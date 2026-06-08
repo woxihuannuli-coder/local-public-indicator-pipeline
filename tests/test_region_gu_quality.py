c59e6435f6ea8c9c870edf0e8aa779520dbcf3ee
김지수 <woxihuannuli@gimjisuui-MacBookPro.local>
2026-06-08 17:28:06 +0900

    test: 잘못된 region_gu 값 감지 테스트 추가

--- tests/test_region_gu_quality.py
+++ tests/test_region_gu_quality.py
@@ -16,3 +16,12 @@ def test_region_gu_not_null_detect_error(small_libraries_df):
     broken_df.loc[0, "region_gu"] = None
     with pytest.raises(AssertionError):
         assert broken_df["region_gu"].notna().all()
+
+def test_invalid_region_gu_value(small_libraries_df):
+    broken_df = small_libraries_df.copy()
+    broken_df.loc[0,"region_gu"] = "팔달구"
+    with pytest.raises(AssertionError):
+        actual_values = set(broken_df["region_gu"].dropna().unique())
+        assert actual_values <= ALLOWED_REGION_GU
+
+
